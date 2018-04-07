9203b53ae6ede8d3e2bfe5e39d8d6ba106b9a56e
Unknown <kimts2002kr@naver.com>
2018-04-07 13:40:30 +0900

    ISSUE-007 Create the login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 
 class LoginPage extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.stste = ({
+      nickname: ""
+    });
+
+    this.onTextChange = this.onTextChange.bind(this);
+    this.onLogin = this.onLogin.bind(this);
+  }
+
   render() {
     return (
       <div>
-
+        <textarea onChange={this.onTextChange}/>
+        <button onClick={this.onLogin}>로그인</button>
       </div>
     );
+  }
+
+  onTextChange(e) {
+    const text = e.target.value;
+    this.setState({nickname: text});
+  }
+
+  onLogin() {
+    alert(this.state.nickname);
   }
 }
 
