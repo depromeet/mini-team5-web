5d5a85ee888216771d0c3e2bb7121394d7968542
Byeong Gyu Choi <bisschoi9541@gmail.com>
2018-04-07 15:04:15 +0900

    Apply stash from dev

--- src/shared/App.jsx
+++ src/shared/App.jsx
@@ -1,12 +1,14 @@
-import React, {Component} from "react";
-import {Route} from "react-router-dom";
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
+import BoardPage from "../pages/BoardPage";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Route exact path="/" component={LoginPage}/>
+        <Route exact path="/" component={LoginPage} />
+        <Route exact path="/boards" component={BoardPage} />
       </div>
     );
   }
