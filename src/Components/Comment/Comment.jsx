2f6ed51ce89a999be15cac2714d8bcc29bb9ba71
Byeong Gyu Choi <bisschoi9541@gmail.com>
2018-04-07 14:17:20 +0900

    Implemented Skeleton of comment page

---
+++ src/Components/Comment/Comment.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Comment extends Component {
+  render() {
+    return (
+      <div>
+        <h3>이것은 바로 댓글</h3>
+      </div>
+    );
+  }
+}
+
+export default Comment;
