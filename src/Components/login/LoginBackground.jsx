9b4afd5a6ff9267b8024f2e0b3fe06334745c5c6
Unknown <kimts2002kr@naver.com>
2018-04-10 02:21:58 +0900

    ISSUE-022 Apply style with assets

--- src/Components/login/LoginBackground.jsx
+++ src/Components/login/LoginBackground.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import bgImg from "../../resources/images/main_01.jpg";
+import loginTitleImg from "../../resources/images/login_title.png";
 
 class LoginBackground extends React.Component {
   render() {
     return (
-      <div className="login-background">
+      <div>
+        <img className="login-background" src={bgImg} alt="메인 화면"/>
+        <img className="login-title" src={loginTitleImg} alt="제목"/>
       </div>
     );
   }
--- src/Components/login/LoginInputLine.jsx
+++ src/Components/login/LoginInputLine.jsx
@@ -6,25 +6,46 @@ class LoginInputLine extends React.Component {
     super(props);
 
     this.state = {
-      nickname: ""
+      nickname: "",
+      isFocused: false,
+      isPlaceHolderDisplay: true
     };
 
     this.onTextChange = this.onTextChange.bind(this);
     this.onLogin = this.onLogin.bind(this);
+    this.onFocus = this.onFocus.bind(this);
+    this.onBlur = this.onBlur.bind(this);
   }
 
   render() {
+    const placeholder = "ENTER\nYOUR\nNAME";
+
     return (
       <div className="login-input-line">
-        <textarea onChange={this.onTextChange} />
-        <button onClick={this.onLogin}>로그인</button>
+        <hr className="login-hr"/>
+        {
+          this.state.isPlaceHolderDisplay
+            ? <div className="login-placeholder" onClick={this.onFocus}>{placeholder}</div>
+            : <input className="login-input" type="text" onChange={this.onTextChange} onBlur={this.onBlur} ref={(input) => {this.nameInput = input;}}/>
+        }
+        <button className="login-button" onClick={this.onLogin}>START</button>
       </div>
     );
   }
 
   onTextChange(e) {
     const text = e.target.value;
-    this.setState({ nickname: text });
+    this.setState({nickname: text});
+  }
+
+  onFocus() {
+    this.setState({isPlaceHolderDisplay: false});
+  }
+
+  onBlur() {
+    if(!this.state.nickname){
+      this.setState({isPlaceHolderDisplay: true});
+    }
   }
 
   async onLogin() {
@@ -37,11 +58,16 @@ class LoginInputLine extends React.Component {
 
     const res = await HttpConnector.login({ nickName: nickname });
     if (res) {
-      alert("success");
       this.props.history.push("boards");
     }
     else {
       alert("서버에서 문제가 발생했습니다!");
+    }
+  }
+
+  componentDidUpdate() {
+    if(!this.state.isPlaceHolderDisplay){
+      this.nameInput.focus();
     }
   }
 }
